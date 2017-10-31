cf2034e27b0a51952e040959dcc4b00503281046
Takashiro <takashiro@qq.com>
2017-10-31 23:41:44 +0800

    Add class PlayerAction

---
+++ src/PlayerAction.cpp
@@ -0,0 +1,39 @@
+/***********************************************************************
+One Night Ultimate Werewolf
+Copyright (C) 2017  Kazuichi Takashiro
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as
+published by the Free Software Foundation, either version 3 of the
+License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+takashiro@qq.com
+************************************************************************/
+
+#include "PlayerAction.h"
+#include "Player.h"
+
+PlayerAction::PlayerAction(PlayerRole role, int priority)
+	: mRole(role)
+	, mPriority(priority)
+{
+}
+
+bool PlayerAction::isEffective(Player *player) const
+{
+	return mRole == player->role();
+}
+
+std::vector<PlayerAction *> &LoadPlayerActions()
+{
+	static std::vector<PlayerAction *> actions;
+	return actions;
+}
