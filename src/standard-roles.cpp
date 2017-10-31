1719692f4560d1e96a1d838ad20f2822f82db390
Takashiro <takashiro@qq.com>
2017-11-01 01:10:57 +0800

    Add standard-roles.cpp

---
+++ src/standard-roles.cpp
@@ -0,0 +1,37 @@
+/***********************************************************************
+One Night Ultimate Werewolf
+Copyright (C) 2017  Kazuichi Takashiro
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as
+published by the Free Software Foundation, either version 3 of the
+License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+takashiro@qq.com
+************************************************************************/
+
+#include "PlayerAction.h"
+
+#include "cmd.h"
+#include "Player.h"
+#include "PlayerRole.h"
+#include "WerewolfDriver.h"
+
+#include <Json.h>
+#include <Room.h>
+
+KA_USING_NAMESPACE
+
+std::vector<PlayerAction *> CreatePlayerActions()
+{
+	std::vector<PlayerAction *> actions;
+	return actions;
+}
